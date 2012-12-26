e1c532be888a07c796e416787731c71dd9a262d1
JackieRamosM <jackie.ljrm@gmail.com>
2012-12-26 13:32:25 -0500

    anadiendo posiciones de la imagen a mover
    esto es un circulo temporalemente

---
+++ ProyectoCaminos/src/Muneco.py
@@ -0,0 +1,11 @@
+'''
+Created on Dec 26, 2012
+
+@author: Lilly
+'''
+from vec2d import *
+
+class Muneco:
+    def __init__(self):
+        self.posicion=vec2d(0,0)
+        self.objetivo=vec2d(0,0)
