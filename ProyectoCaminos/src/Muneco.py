687e070edf68a7afb5355de87a2ff22a637af401
JackieRamosM <jackie.ljrm@gmail.com>
2012-12-27 01:23:37 -0500

    anadiendo atributo imagen

--- ProyectoCaminos/src/Muneco.py
+++ ProyectoCaminos/src/Muneco.py
@@ -4,8 +4,10 @@ Created on Dec 26, 2012
 @author: Lilly
 '''
 from vec2d import *
+import pygame
 
 class Muneco:
     def __init__(self):
+        self.marciano= pygame.image.load("images/personaje.png")
         self.posicion=vec2d(0,0)
         self.objetivo=vec2d(0,0)
