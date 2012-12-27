981661c27f8983b31ef2605a5668cba8265d2fab
Denny K. Schuldt <dschuldt@espol.edu.ec>
2012-12-26 23:55:08 -0500

    Actualizando "Principal"
    Se han agregado validaciones de niveles.

--- ProyectoPython/src/Principal.py
+++ ProyectoPython/src/Principal.py
@@ -19,8 +19,17 @@ def main():
     sprite1.image = imagen1
     sprite1.rect = imagen1.get_rect()    
     sprite1.rect.top = 75
-    sprite1.rect.left = 75    
-         
+    sprite1.rect.left = 75        
+    
+    base1 = False
+    base2 = False
+    base3 = False
+    base4 = False
+    base5 = False
+    base6 = False
+    base7 = False
+    base8 = False
+        
     salir = False
     
     fondo = Fondo()    
@@ -34,35 +43,149 @@ def main():
                     n = 0 
                     while n <= 300:                             
                         sprite1.rect.move_ip(1,0)
-                        pantalla.blit(fondo,(0,0))
+                        #pantalla.blit(fondo,(0,0))
                         pantalla.blit(sprite1.image,sprite1.rect)
                         pygame.display.update()                                                
-                        n = n + 1                                            
+                        n = n + 1                    
+                    y = sprite1.rect.top
+                    x = sprite1.rect.left
+                    
+                    if x == 376 and y == 75 and base1 == False:
+                        print "Reproducir el audio de la Base 1"
+                        base1 = True
+                    elif x == 677 and y == 75 and base2 == False:
+                        print "Reproducir el audio de la Base 2"
+                        base2 = True
+                    elif x == 677 and y == 276 and base3 == False:
+                        print "Reproducir el audio de la Base 3"
+                        base3 = True
+                    elif x == 376 and y == 276 and base4 == False:
+                        print "Reproducir el audio de la Base 4"
+                        base4 = True
+                    elif x == 75 and y == 276 and base5 == False:
+                        print "Reproducir el audio de la Base 5"
+                        base5 = True
+                    elif x == 75 and y == 477 and base6 == False:
+                        print "Reproducir el audio de la Base 6"
+                        base6 = True
+                    elif x == 376 and y == 477 and base7 == False:
+                        print "Reproducir el audio de la Base 7"
+                        base7 = True
+                    elif x == 677 and y == 477 and base8 == False:
+                        print 'Reproducir: "Felicitaciones, has ganado y salvado al planeta de una catastrofe".'
+                        base8 = True                    
+                    print "CoordX: " + str(x) + " y CoordY: " + str(y) 
                 if event.key == pygame.K_LEFT:                                            
                     n = 0 
                     while n <= 300:                             
                         sprite1.rect.move_ip(-1,0)
-                        pantalla.blit(fondo,(0,0))
+                        #pantalla.blit(fondo,(0,0))
                         pantalla.blit(sprite1.image,sprite1.rect)
                         pygame.display.update()                                                
                         n = n + 1
+                    y = sprite1.rect.top
+                    x = sprite1.rect.left
+                    
+                    if x == 376 and y == 75 and base1 == False:
+                        print "Reproducir el audio de la Base 1"
+                        base1 = True
+                    elif x == 677 and y == 75 and base2 == False:
+                        print "Reproducir el audio de la Base 2"
+                        base2 = True
+                    elif x == 677 and y == 276 and base3 == False:
+                        print "Reproducir el audio de la Base 3"
+                        base3 = True
+                    elif x == 376 and y == 276 and base4 == False:
+                        print "Reproducir el audio de la Base 4"
+                        base4 = True
+                    elif x == 75 and y == 276 and base5 == False:
+                        print "Reproducir el audio de la Base 5"
+                        base5 = True
+                    elif x == 75 and y == 477 and base6 == False:
+                        print "Reproducir el audio de la Base 6"
+                        base6 = True
+                    elif x == 376 and y == 477 and base7 == False:
+                        print "Reproducir el audio de la Base 7"
+                        base7 = True
+                    elif x == 677 and y == 477 and base8 == False:
+                        print 'Reproducir: "Felicitaciones, has ganado y salvado al planeta de una catastrofe".'
+                        base8 = True   
+                        
+                    print "CoordX: " + str(x) + " y CoordY: " + str(y)
                 if event.key == pygame.K_UP:                                            
                     n = 0 
                     while n <= 200:                             
                         sprite1.rect.move_ip(0,-1)
-                        pantalla.blit(fondo,(0,0))
+                        #pantalla.blit(fondo,(0,0))
                         pantalla.blit(sprite1.image,sprite1.rect)
                         pygame.display.update()                                                
                         n = n + 1
+                    y = sprite1.rect.top
+                    x = sprite1.rect.left
+
+                    if x == 376 and y == 75 and base1 == False:
+                        print "Reproducir el audio de la Base 1"
+                        base1 = True
+                    elif x == 677 and y == 75 and base2 == False:
+                        print "Reproducir el audio de la Base 2"
+                        base2 = True
+                    elif x == 677 and y == 276 and base3 == False:
+                        print "Reproducir el audio de la Base 3"
+                        base3 = True
+                    elif x == 376 and y == 276 and base4 == False:
+                        print "Reproducir el audio de la Base 4"
+                        base4 = True
+                    elif x == 75 and y == 276 and base5 == False:
+                        print "Reproducir el audio de la Base 5"
+                        base5 = True
+                    elif x == 75 and y == 477 and base6 == False:
+                        print "Reproducir el audio de la Base 6"
+                        base6 = True
+                    elif x == 376 and y == 477 and base7 == False:
+                        print "Reproducir el audio de la Base 7"
+                        base7 = True
+                    elif x == 677 and y == 477 and base8 == False:
+                        print 'Reproducir: "Felicitaciones, has ganado y salvado al planeta de una catastrofe".'
+                        base8 = True   
+                        
+                    print "CoordX: " + str(x) + " y CoordY: " + str(y)
                 if event.key == pygame.K_DOWN:                                            
                     n = 0 
                     while n <= 200:                             
                         sprite1.rect.move_ip(0,1)
-                        pantalla.blit(fondo,(0,0))
+                        #pantalla.blit(fondo,(0,0))
                         pantalla.blit(sprite1.image,sprite1.rect)
                         pygame.display.update()                                                
                         n = n + 1                            
-                            
+                    y = sprite1.rect.top
+                    x = sprite1.rect.left
+                    
+                    if x == 376 and y == 75 and base1 == False:
+                        print "Reproducir el audio de la Base 1"
+                        base1 = True
+                    elif x == 677 and y == 75 and base2 == False:
+                        print "Reproducir el audio de la Base 2"
+                        base2 = True
+                    elif x == 677 and y == 276 and base3 == False:
+                        print "Reproducir el audio de la Base 3"
+                        base3 = True
+                    elif x == 376 and y == 276 and base4 == False:
+                        print "Reproducir el audio de la Base 4"
+                        base4 = True
+                    elif x == 75 and y == 276 and base5 == False:
+                        print "Reproducir el audio de la Base 5"
+                        base5 = True
+                    elif x == 75 and y == 477 and base6 == False:
+                        print "Reproducir el audio de la Base 6"
+                        base6 = True
+                    elif x == 376 and y == 477 and base7 == False:
+                        print "Reproducir el audio de la Base 7"
+                        base7 = True
+                    elif x == 677 and y == 477 and base8 == False:
+                        print 'Reproducir: "Felicitaciones, has ganado y salvado al planeta de una catastrofe".'
+                        base8 = True   
+                    
+                    print "CoordX: " + str(x) + " y CoordY: " + str(y)
         reloj1.tick(20)
         pantalla.blit(fondo,(0,0))
         pantalla.blit(sprite1.image,sprite1.rect)        
